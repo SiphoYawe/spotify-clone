a71335773e5d6604ff2dcc0d3185fb120dd0d204
SiphoYawe <yawephillip@gmail.com>
2024-03-30 21:13:54 +0000

    Header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,17 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
+
 export default function Home() {
-	return <div className=" text-green-500">Main Content</div>;
+	return (
+		<div className=" h-full w-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
+			<Header>
+				<div className="mb-2">
+					<h1 className="text-3xl font-semibold text-white">Welcome Back</h1>
+				</div>
+				<div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+					<ListItem />
+				</div>
+			</Header>
+		</div>
+	);
 }
